ae4cfedc4f0b8e70ca6cfb4328010e5e5a24670d
mj727 <madiejones727@gmail.com>
2022-10-03 20:14:34 -0600

    Finished singly linked list

--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,73 @@
-#include <stdio.h>
+// Singly Linked list converted from Java to C 
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node 
+{
+    int data;
+    struct Node *next;
+
+}NODE;
+
+NODE *newNode(int num) 
+{
+    NODE* result = malloc(sizeof(NODE));
+
+    if(result != NULL) 
+    {
+        result -> data = num;
+        result -> next = NULL;
+    }
+
+    return result;
+}
+
+NODE *head;
+NODE *tail;
+
+void addNode(int data) 
+{
+    NODE* cNode = newNode(data);
+    if(head == NULL) 
+    {
+        head = cNode;
+        tail = cNode;
+    } 
+
+    else 
+    {
+        tail -> next = cNode;
+        tail = cNode;
+    }
+}
+
+void display() 
+{
+    NODE* current = head;
+
+    if(head == NULL) 
+    {
+        printf("Empty list");
+        return;
+    }
+    printf("Singly linked list nodes: \n");
+
+    while(current != NULL) 
+    {
+        printf("%i \n", current -> data);
+        current = current -> next;
+    }
+    printf("\n");
+}
+ 
+int main() 
+{
+    addNode(1);
+    addNode(2);
+    addNode(3);
+    addNode(4);
+
+    display();
     return 0;
 }
